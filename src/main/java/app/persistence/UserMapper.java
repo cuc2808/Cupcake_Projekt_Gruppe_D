03d54fe5231d1218f562b4e68c7f18dacf4d8775
NicolaiFaellesskov <nicolainoah@gmail.com>
2026-04-09 11:43:28 +0200

    Tilføjet UserMapper Indenholder: Login createUser

---
+++ src/main/java/app/persistence/UserMapper.java
@@ -0,0 +1,52 @@
+package app.persistence;
+
+
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper {
+    public static User login(String username, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "select * from users where username =? and password =?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+        ) {
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("user_id");
+                double balance = rs.getDouble("balance");
+                boolean administrator = rs.getBoolean("administrator");
+
+                return new User(id, username, password, balance, administrator);
+            } else {
+                throw new DatabaseException("Fejl i login, prøv igen");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Connection to db dosen't work", e.getMessage());
+        }
+    }
+
+    public static void createUser(String username, String password, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "INSERT INTO users (username, password) VALUES(?,?)";
+
+        try
+                (
+                        Connection connection = connectionPool.getConnection();
+                        PreparedStatement ps = connection.prepareStatement(sql);
+                ) {
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseException("Fejl ved oprettelse", e.getMessage());
+        }
+    }
+}
